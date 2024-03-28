1e94731ff424280d3dfbf5efba7e2e8033995c2c
Swifty1235 <104468694+Swifty1235@users.noreply.github.com>
2024-03-28 17:27:09 -0400

    Initial Commit

---
+++ Emoji/EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiApp.swift
+//  Emoji
+//
+//  Created by Pedro Romero on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
