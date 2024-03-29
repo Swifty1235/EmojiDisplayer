665571d9ff4c817d43ba1ea33ea51afa8713169d
Swifty1235 <104468694+Swifty1235@users.noreply.github.com>
2024-03-29 14:41:48 -0400

    Emoji lovers

--- Emoji/ContentView.swift
+++ Emoji/ContentView.swift
@@ -7,18 +7,33 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 🐉, 👨🏻‍🦳, 🧐, 🦧
+}
+
 struct ContentView: View {
+    @State var selecting: Emoji = .🐉
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selecting.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selecting) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.palette)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
